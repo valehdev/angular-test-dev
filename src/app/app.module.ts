0e033795b9800c29eb08d0d6dc17be4beb07b32b
Asadli Valeh Gudrat <ValehA@BankRespublika.az>
2017-12-29 17:43:08 +0400

    ng init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { UsersComponent } from './users/users.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { AppRountingModule } from './/app-rounting.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  	declarations: [
+		AppComponent,
+		HeroesComponent,
+		UsersComponent,
+		HeroDetailComponent,
+		MessagesComponent,
+		DashboardComponent,
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRountingModule
+	],
+	providers: [ 
+		HeroService, 
+		MessageService 
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { USERS } from '../mock-users';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+	users = USERS;
+
+	selectedUser: User;
+
+	onSelect(user: User): void {
+		this.selectedUser = user;
+	}
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
